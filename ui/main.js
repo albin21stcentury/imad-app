b64ee4c109396fe28a5a0474a7b407cad9eec2b6
Albin Pius <albinpius12@gmail.com>
2017-12-28 21:04:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,7 +21,7 @@ request.send(null);
 var nameinp=document.getElementById("name");
 var submit=document.getElementById("submit");
 submit.onclick=function() {
-    var names={'name1','name2'};
+    var names = ['name1','name2'];
     var list='';
     for(var i=0;i<names.length;i++) {
         list+='<li>'+names[i]+'</li>';
